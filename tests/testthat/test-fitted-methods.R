257dfbd52ecbf09401af9cfb6ca330313ee2f408
johnrbryant <john.r.bryant@gmail.com>
2024-03-19 11:59:40 +1300

    modified n_draw

--- tests/testthat/test-fitted-methods.R
+++ tests/testthat/test-fitted-methods.R
@@ -64,14 +64,30 @@ test_that("'components' throws correct error with invalid 'what'", {
 
 ## 'n_draw' -------------------------------------------------------------------
 
-test_that("'n_draw' works", {
+test_that("'n_draw' works - non-list inputs", {
   set.seed(0)
   data <- expand.grid(time = 2001:2010, sex = c("F", "M"), region = c("A", "B"))
   data$y <- rnorm(n = nrow(data))
   fit <- fit_ts(data = data,
                 indvar = "y",
                 byvar = c("sex", "region"))
-  expect_identical(fit$n_draw, 1000L)
+  expect_identical(n_draw(fit), 1000L)
   n_draw(fit) <- 1L
-  expect_identical(fit$n_draw, 1L)
+  expect_identical(n_draw(fit), 1L)
 })
+
+test_that("'n_draw' works - list inputs", {
+  set.seed(0)
+  data <- expand.grid(time = 2001:2005, sex = c("F", "M"))
+  data$y <- replicate(n = nrow(data), list(rnorm(10)))
+  fit <- fit_ts(data = data,
+                indvar = "y",
+                byvar = "sex")
+  expect_identical(n_draw(fit), 10L)
+  n_draw(fit) <- 5L
+  expect_identical(n_draw(fit), 5L)
+  expect_identical(length(fit$data[["y"]][[1L]]), 5L)
+  expect_error(n_draw(fit) <- 10,
+               "replacement value for 'n_draw' \\[10\\] greater than number of draws in inputs \\[5\\]")
+})
+
